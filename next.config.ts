2e566e6bbb1d203025decf8aa18753e91d6c1584
Chamika Jayarathna <failgames.lk@gmail.com>
2026-08-30 20:33:05 +0530

    🐳 feat: dockerize the entire project

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,8 @@ import path from "node:path";
 import "./src/shared/config/env";
 
 const nextConfig: NextConfig = {
+  output: "standalone",
+  outputFileTracingRoot: path.join(__dirname),
   turbopack: {
     root: __dirname,
   },
